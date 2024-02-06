911801a4962370eec0a8bde2efbb4aff5d6949e4
AkshayDeosthale <akshay.deosthale6@gmail.com>
2024-02-06 17:38:03 +0530

    [FIX] ADD project

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import timetrack from "@/public/thumbnails/client.webp";
 import medium from "@/public/thumbnails/medium.webp";
 import school from "@/public/thumbnails/school.webp";
 import task_manage from "@/public/thumbnails/task-manage.png";
+import gigma from "@/public/thumbnails/gigma.png";
 import DIS from "@/public/thumbnails/DIS.png";
 
 import React from "react";
@@ -73,25 +74,12 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "Bright Children Montessori Academy",
+    title: "Gigma Not Figma",
     description:
-      "I served as a full-stack developer for a prominent Canadian school's website, contributing my expertise on a freelance basis.",
-    tags: ["TypeScript", "Next.js", "PostgresSQL", "Tailwind", "Strapi"],
-    imageUrl: school,
-  },
-  {
-    title: "Medium Blog clone",
-    description:
-      "A streamlined Medium clone, merging effortless content creation with a curated reading journey for contemporary digital storytellers.",
-    tags: ["TypeScript", "Next.js", "Tailwind", "Redux", "Sanity.IO"],
-    imageUrl: medium,
-  },
-  {
-    title: "TimeTrak Scheduling Software",
-    description:
-      "A sleek web application for optimized commercial tracking, boosting productivity and efficiency in all business operations.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: timetrack,
+      "Created a collaborative design tool akin to Figma using Next.js, empowering real-time collaboration and design iteration.",
+    tags: ["Next.js", "FabricJS", "Liveblocks.io", "TS", "shadCN"],
+    imageUrl: gigma,
+    url: "https://gigma-not-figma.akshaydeosthale.com",
   },
   {
     title: "Delhi International School",
@@ -99,6 +87,32 @@ export const projectsData = [
       "Crafted DIS website using Next.js, Nest.js, and CSS, blending modern frontend design with a robust backend for an immersive user experience.",
     tags: ["Next.js", "NestJS"],
     imageUrl: DIS,
+    url: "https://disdelhi.com",
+  },
+  {
+    title: "Bright Children Montessori Academy",
+    description:
+      "I served as a full-stack developer for a prominent Canadian school's website, contributing my expertise on a freelance basis.",
+    tags: ["TypeScript", "Next.js", "PostgresSQL", "Tailwind", "Strapi"],
+    imageUrl: school,
+    url: "https://children-school.akshaydeosthale.com",
+  },
+  {
+    title: "Medium Blog clone",
+    description:
+      "A streamlined Medium clone, merging effortless content creation with a curated reading journey for contemporary digital storytellers.",
+    tags: ["TypeScript", "Next.js", "Tailwind", "Redux", "Sanity.IO"],
+    imageUrl: medium,
+    url: "https://medium-blogs.akshaydeosthale.com",
+  },
+
+  {
+    title: "TimeTrak Scheduling Software",
+    description:
+      "A sleek web application for optimized commercial tracking, boosting productivity and efficiency in all business operations.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: timetrack,
+    url: undefined,
   },
   {
     title: "Issue Management System",
@@ -106,6 +120,7 @@ export const projectsData = [
       "Web application to track projects an its issues, issue can be created searched deleted or marked as completed",
     tags: ["React", "Next.js", "Tailwind", "Framer", "Server Actions", "SSR"],
     imageUrl: task_manage,
+    url: undefined,
   },
 ] as const;
 
